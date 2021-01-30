c190e0b8e5a23e70b5f551120c1419d934b68960
NEFİSE HAZIR <2019555034@ogr.cu.edu.tr>
2021-01-30 15:01:45 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public string Id { get; set; }
+        public string Ad { get; set; }
+        public int Yaş { get; set; }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "AHMET";
+            musteri1.Yaş = 56;
+            musteri1.Id = "234567";
+
+            Musteri musteri2 = new Musteri();
+            musteri1.Ad = "mehme";
+            musteri1.Id = "12345678";
+            musteri1.Yaş = 34;;
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            
+                
+        }
+    }
+}
